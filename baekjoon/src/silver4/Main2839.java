4130d3224f4d039dfa886397fce856d5692dbe17
jaeeun25 <ljhsc00@naver.com>
2023-03-05 21:37:50 +0900

    2023-03-05

---
+++ baekjoon/src/silver4/Main2839.java
@@ -0,0 +1,35 @@
+package silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2839 {
+	// 백준 2839번: 설탕 배달
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int suga = Integer.parseInt(br.readLine());
+		int cnt = -1, i = 0;
+		
+		while(true) {
+
+			if(suga < 0){
+				break;
+			}
+			else if(suga == 0) {
+				cnt = i;
+				break;
+			}
+			else if(suga <= 12 && suga % 3 == 0) {
+				cnt = suga/3 + i;
+				break;
+			}
+			suga -= 5;
+			i++;
+		}
+		System.out.println(cnt);
+	}
+}
---
+++ baekjoon/src/silver5/Main1193.java
@@ -0,0 +1,46 @@
+package silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1193 {
+	// 백준 1193: 분수 찾기
+	// 입력받은 번호가 몇번째 라인에 속하는지, 짝수 라인인지 홀수 라인인지를 알아내야 한다.
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int line = 1;
+		int x=0, y=0;
+		
+		while(true) {
+			if(n <= line*(line+1)/2) {
+				if(line%2 == 0) {
+					x = n - (line*(line-1)/2);	// line*(line-1)/2 -> 아래층까지의 분수들 개수
+					y = line + 1 - x;
+				}
+				else {
+					y = n - (line*(line-1)/2);
+					x = line + 1 - y;
+				}
+				break;
+			}
+			
+			line++;
+		}
+		
+		System.out.println(x + "/" + y);
+		
+		br.close();
+		
+		/* 다른방법
+		 while(x > line){
+		 	x -= line;
+		 	line++;
+		 }
+		 */
+	}
+}
