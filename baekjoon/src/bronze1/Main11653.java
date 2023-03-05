f5c7701bf36ca36cd64e6f9e6df7e90f28d4c4be
jaeeun25 <ljhsc00@naver.com>
2023-03-06 01:18:02 +0900

    2023-03-06

---
+++ baekjoon/src/bronze1/Main11653.java
@@ -0,0 +1,30 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main11653 {
+	// 백준 11653번: 소인수분해
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int i = 2;
+		
+		while(true) {
+			if(n == 1 || n == 0) break;
+			if(n % i == 0) {
+				n /= i;
+				System.out.println(i);
+			}
+			else {
+				i++;
+			}
+		}
+				
+		br.close();
+	}
+}
---
+++ baekjoon/src/bronze1/Main9506.java
@@ -0,0 +1,40 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main9506 {
+	// 백준 9506번: 약수들의 합
+	// String Builder 이용해 주로 품.
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = 0, result;
+		String str = null, str2;
+		
+		while(true) {
+			n = Integer.parseInt(br.readLine());
+			result = 1;
+			str2 = "";
+			
+			if(n == -1) break;
+			
+			for(int i=2; i<n; i++) {
+				if(n % i == 0) {
+					result += i;
+					str2 += " + " + i;
+				}
+			}
+			if(n == result) str = n + " = 1" + str2;
+			else str = n + " is NOT perfect.";
+			
+			System.out.println(str);
+		}
+		
+		br.close();
+		
+	}
+
+}
