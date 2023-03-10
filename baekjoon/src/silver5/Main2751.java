b5bf5cb3ca2cba181cc682ef59c6624640e5f882
jaeeun25 <ljhsc00@naver.com>
2023-03-10 23:55:29 +0900

    2023-03-10

---
+++ baekjoon/src/silver5/Main2751.java
@@ -0,0 +1,20 @@
+package silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2751 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i<n;i++) {
+			
+		}
+		
+	}
+}	
